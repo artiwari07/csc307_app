31c6d097c314f8f540861849bf6f223258cb6509
artiwari07 <artiwari@calpoly.edu>
2024-01-16 11:10:11 -0800

    Finished first assignment

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -6,13 +6,17 @@ import Form from "./Form";
 function MyApp() {
     const [characters, setCharacters] = useState([]);
 
+    function updateList(person) {
+      setCharacters([...characters, person]);
+    }
+
     return (
       <div className="container">
         <Table
           characterData={characters}
           removeCharacter={removeOneCharacter}
         />
-        <Form />
+        <Form handleSubmit={updateList} />
       </div>
     );
 
@@ -23,5 +27,6 @@ function removeOneCharacter(index) {
     setCharacters(updated);
   }
 }
+
 
 export default MyApp;
