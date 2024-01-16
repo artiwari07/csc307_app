e462c440a6edbf989a76b2c68cce554d785f23e3
artiwari07 <artiwari@calpoly.edu>
2024-01-16 09:53:16 -0800

    Finished Step 12

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -1,23 +1,20 @@
 // src/MyApp.js
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 function MyApp() {
-    const [characters, setCharacters] = useState([
-        {
-          name: "Charlie",
-          job: "Janitor" // the rest of the data
-        }
-      ]);
+    const [characters, setCharacters] = useState([]);
 
-      return (
-        <div className="container">
-          <Table
-            characterData={characters}
-            removeCharacter={removeOneCharacter}
-          />
-        </div>
-      );
+    return (
+      <div className="container">
+        <Table
+          characterData={characters}
+          removeCharacter={removeOneCharacter}
+        />
+        <Form />
+      </div>
+    );
 
 function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
