4960483d9749f8c7879838e949fd4d6d95905e96
artiwari07 <artiwari@calpoly.edu>
2024-01-31 18:32:09 -0800

    Finished Exercise 3

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -6,12 +6,27 @@ import Form from "./Form";
 function MyApp() {
     const [characters, setCharacters] = useState([]);
 
-    function updateList(person) { 
+    function updateList(person) {
       postUser(person)
-        .then(() => setCharacters([...characters, person]))
-        .catch((error) => {
-          console.log(error);
+        .then((response) => {
+          if (response.status === 201) {
+            console.log("User added successfully! Status code:", response.status);
+            // Only attempt to parse JSON if the status is 201
+            return response.json();
+          } else {
+            console.log("Failed to add user. Status code:", response.status);
+            // Don't attempt to parse JSON if the status is not 201
+            throw new Error("Failed to add user");
+          }
         })
+        .then((addedUser) => {
+          console.log("Generated ID:", addedUser.id);
+          // Only update characters if JSON parsing is successful
+          setCharacters([...characters, addedUser]);
+        })
+        .catch((error) => {
+          console.error("Error adding user:", error);
+        });
     }
 
     function fetchUsers() {
@@ -31,6 +46,34 @@ function MyApp() {
       return promise;
     }
 
+    function removeOneCharacter(id, index) {
+      fetch(`http://localhost:8000/users/${id}`, {
+        method: "DELETE",
+      })
+        .then((response) => {
+          if (response.status === 204) {
+            console.log("User deleted successfully! Status code:", response.status);
+          } 
+          else if (response.status === 404) {
+            console.log("User not found. Status code:", response.status);
+            throw new Error("User not found");
+          } 
+          else {
+            console.log("Failed to delete user. Status code:", response.status);
+            throw new Error("Failed to delete user");
+          }
+        })
+        .then((deletedUser) => {
+          // Wait for the DELETE request to be successful on the backend before updating the frontend
+          const updatedCharacters = characters.filter((_, i) => i !== index);
+          setCharacters(updatedCharacters);
+          console.log("Deleted User:", deletedUser);
+        })
+        .catch((error) => {
+          console.error("Error deleting user:", error);
+        });
+    }   
+    
     useEffect(() => {
       fetchUsers()
         .then((res) => res.json())
@@ -48,12 +91,7 @@ function MyApp() {
       </div>
     );
 
-function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => {
-      return i !== index;
-    });
-    setCharacters(updated);
-  }
+    
 }
 
 
--- packages/react-frontend/src/Table.js
+++ packages/react-frontend/src/Table.js
@@ -1,49 +1,47 @@
 // src/Table.js
 
 function TableHeader() {
-    return (
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Job</th>
-        </tr>
-      </thead>
-    );
-  }
-  
-  function TableBody(props) {
-    const rows = props.characterData.map((row, index) => {
-      return (
-        <tr key={index}>
-          <td>{row.name}</td>
-          <td>{row.job}</td>
-          <td>
-    <button onClick={() => props.removeCharacter(index)}>
-      Delete
-    </button>
-  </td>
-</tr>
-      );
-     }
-    );
-    return (
-        <tbody>
-          {rows}
-         </tbody>
-     );
-  }
-  
-  function Table(props) {
-    return (
-      <table>
-        <TableHeader />
-        <TableBody
-          characterData={props.characterData}
-          removeCharacter={props.removeCharacter}
-        />
-      </table>
-    );
-  }
+  return (
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Action</th> {/* Added Action column header */}
+      </tr>
+    </thead>
+  );
+}
 
+function TableBody(props) {
+  const rows = props.characterData.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{row.id}</td>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(row.id, index)}>
+            Delete
+          </button>
+        </td>
+      </tr>
+    );
+  });
 
-  export default Table;
+  return <tbody>{rows}</tbody>;
+}
+
+function Table(props) {
+  return (
+    <table>
+      <TableHeader />
+      <TableBody
+        characterData={props.characterData}
+        removeCharacter={props.removeCharacter}
+      />
+    </table>
+  );
+}
+
+export default Table;
